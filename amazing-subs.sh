82519ff132da677be5a0f4ea193ab0ab0e50efa6
zerone0x1 <just4learn0@gmail.com>
2024-09-29 11:21:48 +0300

    Update amazing-subs.sh

--- amazing-subs.sh
+++ amazing-subs.sh
@@ -5,7 +5,7 @@ curl -s "https://jldc.me/anubis/subdomains/$1" | grep -Po "((http|https):\/\/)?(
 curl -s "https://otx.alienvault.com/api/v1/indicators/domain/$1/url_list?limit=100&page=1" | grep -o '"hostname": *"[^"]*' | sed 's/"hostname": "//' | sort -u >2.txt
 curl -s "https://api.hackertarget.com/hostsearch/?q=$1" | sed 's/,.*//'  >3.txt
 curl -s "https://jldc.me/anubis/subdomains/$1" | grep -Po "((http|https):\/\/)?(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | sort -u >4.txt
-subfinder -d $1 -o 5.txt
+subfinder -d $1 -all -o 5.txt
 assetfinder -subs-only $1 > 6.txt
 cat 1.txt 2.txt 3.txt 4.txt 5.txt 6.txt >all-subs.txt
 httpx -l all-subs.txt -o live.txt
